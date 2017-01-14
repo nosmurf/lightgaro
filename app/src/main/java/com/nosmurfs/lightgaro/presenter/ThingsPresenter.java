fa3489adc045df0b43cd72e5c03e10165219785b
Sergio Casero <sergio.casero23@gmail.com>
2017-01-14 01:50:16 +0100

    Added gpio initialization

--- app/src/main/java/com/nosmurfs/lightgaro/presenter/ThingsPresenter.java
+++ app/src/main/java/com/nosmurfs/lightgaro/presenter/ThingsPresenter.java
@@ -2,8 +2,11 @@ package com.nosmurfs.lightgaro.presenter;
 
 import android.util.Log;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,6 +17,14 @@ public class ThingsPresenter extends Presenter<ThingsPresenter.View> {
 
     private static final String TAG = "ThingsPresenter";
 
+    private static final int MAX_RELAYS = 8;
+
+    private List<Gpio> relays;
+
+    public ThingsPresenter() {
+        relays = new ArrayList<>();
+    }
+
     @Override
     protected void initialize() {
         PeripheralManagerService peripheralManagerService = new PeripheralManagerService();
@@ -21,16 +32,41 @@ public class ThingsPresenter extends Presenter<ThingsPresenter.View> {
         if (portList.isEmpty()) {
             Log.i(TAG, "No GPIO port available on this device.");
         } else {
-            Log.i(TAG, "List of available ports: " + portList);
+            try {
+                initializeGpios(peripheralManagerService, portList, portList.size() < MAX_RELAYS ? portList.size() : MAX_RELAYS);
+            } catch (IOException e) {
+                view.showError("An error has ocurred");
+            }
+        }
+    }
+
+    private void initializeGpios(PeripheralManagerService peripheralManagerService, List<String> portList, int size) throws IOException {
+        for (int index = 0; index < size; index++) {
+            String port = portList.get(index);
+            Gpio gpio = peripheralManagerService.openGpio(port);
+            gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            gpio.setActiveType(Gpio.ACTIVE_LOW);
+            gpio.setValue(true);
+
+            relays.add(gpio);
+
+            // TODO: 14/01/2017 show relays in screen for help user
+            Log.i(TAG, "initializeGpios: Relay " + index + " into " + port);
         }
     }
 
     @Override
     public void destroy() {
-        // TODO: 11/01/2017
+        try {
+            for (Gpio relay : relays) {
+                relay.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public interface View extends Presenter.View{
-        // TODO: 11/01/2017  
+        // TODO: 11/01/2017
     }
 }
