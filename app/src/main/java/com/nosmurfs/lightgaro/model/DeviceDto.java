8469415173465c54b0f0813b0db0ea76cc083010
Sergio Casero <sergio.casero23@gmail.com>
2017-01-26 22:50:02 +0100

    Added dtos for provide login

---
+++ app/src/main/java/com/nosmurfs/lightgaro/model/DeviceDto.java
@@ -0,0 +1,15 @@
+package com.nosmurfs.lightgaro.model;
+
+import java.util.List;
+
+/**
+ * Created by Sergio on 26/01/2017.
+ */
+
+public class DeviceDto {
+    private String uniqueId;
+
+    private List<RelayDto> relays;
+
+    private UserDto user;
+}
---
+++ app/src/main/java/com/nosmurfs/lightgaro/model/UserDto.java
@@ -0,0 +1,51 @@
+package com.nosmurfs.lightgaro.model;
+
+/**
+ * Created by Sergio on 26/01/2017.
+ */
+
+public class UserDto {
+    private String name;
+
+    private String email;
+
+    private String imageUrl;
+
+    private boolean binded;
+
+    public UserDto(boolean binded) {
+        this.binded = binded;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isBinded() {
+        return binded;
+    }
+
+    public void setBinded(boolean binded) {
+        this.binded = binded;
+    }
+}
